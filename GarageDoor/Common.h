b6603b6eddf311b20d0c91e918dde515812bade7
Ricardo Verlezza <kalua24@msn.com>
2013-03-10 13:38:57 -0400

    Moved activateDoor and processCommands to Common.h.
    Removed DOOR_SENSOR_TYPE, since it is not needed.

--- GarageDoor/Common.h
+++ GarageDoor/Common.h
@@ -10,56 +10,8 @@
 //
 //***********************************************************************
 
-boolean isHigh(int pin) {
-  if ((analogRead(pin) * analogVoltageCoeff) > analogHigh)
-    return true;
-  else
-    return false;
-}
-
 //***************************** LEFT DOOR *************************************************************************
-#if DOOR_SENSOR_TYPE == 1
-void leftStatusDoorUnknownHigh(boolean &DoorClosed, boolean &DoorOpen) {
-  DoorClosed = false;
-  DoorOpen = false;
-}
-
-void leftStatusDoorUnknownLow(boolean &DoorClosed, boolean &DoorOpen) {
-  DoorClosed = false;
-  DoorOpen = false;
-}
-
-void leftStatusDoorOpen(int doors, boolean &DoorClosed, boolean &DoorOpen) {
-  DoorClosed = false;
-  DoorOpen = true;
-
-  if (doors == 2 && (leftState == 0 || leftState == 20)) { //JARED
-    //tweet("Left Door Opened @ ");
-    //Serial.println("Left door opened");
-  } 
-  else if (leftState == 0 || leftState == 20) { //JARED
-    //tweet("Opened @ ");
-    //Serial.println("opened");
-  }
-}
-
-void leftStatusDoorClosed(int doors, boolean &DoorClosed, boolean &DoorOpen) {
-  DoorClosed = true;
-  DoorOpen = false;
-
-  if (doors == 2 && (leftState == 0 || leftState == 60)) { //JARED
-    //tweet("Left Door Closed @ ");
-    //Serial.println("Left door closed");
-  } 
-  else if (leftState == 0 || leftState == 60) { //JARED
-    //tweet("Closed @ ");
-    //Serial.println("closed");
-  }
-}
-#endif
-
 void leftStatus(){
-#if DOOR_SENSOR_TYPE == 0
   //get garage door status
   if ((digitalRead(openSensor1) == LOW) && (DoorOpen != true)) { //open
     DoorClosed = false;
@@ -88,28 +40,12 @@ void leftStatus(){
       DoorOpen = false;
     }
   }
-#elif DOOR_SENSOR_TYPE == 1
-  if ((isHigh(analogOpenSensor1) == false) && (DoorOpen != true)) {
-    leftStatusDoorOpen(doors, DoorOpen, DoorClosed);
-  } 
-  else if ((isHigh(analogCloseSensor1) == false) && (DoorClosed != true)) {
-    leftStatusDoorClosed(doors, DoorOpen, DoorClosed);
-  } 
-  else if ((isHigh(analogOpenSensor1) == isHigh(analogCloseSensor1)) && (DoorClosed == true || DoorOpen == true)) {
-    delay(250);
-    if (isHigh(analogOpenSensor1) == isHigh(analogCloseSensor1)) {
-      leftStatusDoorUnknownHigh(DoorOpen, DoorClosed);
-    }
-  }
-
-#endif
 }
 
 
 
 //***************************************** RIGHT DOOR ******************************************
 void rightStatus() {
-  #if DOOR_SENSOR_TYPE == 0
   //get door 2 status
   if (doors == 2) {
     if ((digitalRead(openSensor2) == LOW) && (Door2Open != true)) {//open
@@ -132,30 +68,111 @@ void rightStatus() {
       }
     }
   }
-  #elif DOOR_SENSOR_TYPE == 1
-  //get door 2 status
-  if (doors == 2) {
-    if ((digitalRead(analogOpenSensor2) == LOW) && (Door2Open != true)) {//open
-      Door2Closed = false;
-      Door2Open = true;
-      if (rightState == 0 || rightState == 20)
-      Serial.println("Right door opened");
-    }
-    else if ((digitalRead(analogCloseSensor2) == LOW) && (Door2Closed != true)) {//closed
-      Door2Closed = true;
-      Door2Open = false;
-      if (rightState == 0 || rightState == 60)
-      Serial.println("Right door closed");
-    }
-    else if (digitalRead(analogCloseSensor2) == HIGH && digitalRead(analogOpenSensor2) == HIGH && (Door2Closed == true || Door2Open == true)) {
-      delay(250);
-      if (digitalRead(analogCloseSensor2) == HIGH && digitalRead(analogOpenSensor2) == HIGH) {
-        Door2Closed = false;
-        Door2Open = false;
-      }
-    }
-  }
-  #endif
 }
 
+//*****************************ACTIVATE COMMAND***************************************
+int activateDoor(int *doorState, int action,  unsigned long *stateTimer, boolean closed, boolean open, int buttonOutput)  { //JARED
+  switch (*doorState) {
+  case 0: //Default state.  If action is 1, begin opening door.  If action is 2, begin closing door.
+    if (action == 1) *doorState = 10; //Opening Door
+    if (action == 2) *doorState = 50; //Closing Door
+    *stateTimer = millis(); //entire process can take 20 seconds total
+    break;
+
+  case 10: //Door is idle.  If door not already open, trigger button, record timestamp, and move to next case.
+    if (open != true) { 
+      digitalWrite(buttonOutput, HIGH);
+      delay(500);
+      digitalWrite(buttonOutput, LOW);
+      delay(500);
+      *doorState = 20;
+    }
+    else if (open == true && closed == false) {
+      *doorState = 0;
+    }
+    break;
+
+  case 20: //Door is moving.  Wait for open or closed.  If open, set case to 0.  If closed, set case to 10.   If timeout expired, return 1
+    if (open == true && closed == false) *doorState = 0;
+    else if (millis() - *stateTimer > 20000) *doorState = 100;
+    else if (closed == true) *doorState = 10;
+    break;
+
+  case 50: //Door is idle.  If door not already closed, trigger button, record timestamp, and move to next case.
+    if (closed != true) { 
+      digitalWrite(buttonOutput, HIGH);
+      delay(500);
+      digitalWrite(buttonOutput, LOW);
+      delay(500);
+      *doorState =60;
+    }
+    else if (open == false && closed == true) {
+      *doorState = 0;
+    }
+    break;
+
+  case 60: //Door is moving.  Wait for open or closed.  If closed, set case to 0.  If oo, set case to 10.   If timeout expired, return 1
+    if (closed == true && open == false) *doorState = 0;
+    else if (millis() - *stateTimer > 20000) *doorState = 100;
+    else if (open == true) *doorState = 50;
+    break;
+
+  case 100: //Shit's wrong.  If action == 3, the error is ack'd
+    digitalWrite(errorLED, HIGH);
+    if (action == 3) *doorState = 0; //Ack'd
+    break;
+  }
+
+  return *doorState;
+}
+
+//*****************************PROCESS COMMANDS***************************************
+void processCommands() {
+  switch (cmd) {
+  case 1:
+    //Open command
+    if (leftState == 0) activateDoor(&leftState, 1, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
+    break;
+  case 2:
+    //Close command
+    if (leftState == 0) activateDoor(&leftState, 2, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
+    break;
+  case 3:
+    //toggle command
+    if (leftState == 0) { //JARED
+      digitalWrite(ledPin, HIGH);
+      delay(500);
+      digitalWrite(ledPin, LOW);
+    }
+    break;
+  case 4:
+    //Open door 2 command
+    if (rightState == 0) activateDoor(&rightState, 1, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+    break;
+  case 5:
+    //close door 2 command
+    if (rightState == 0) activateDoor(&rightState, 2, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+    break;
+  case 6:
+    //toggle door 2 command
+    if (rightState == 0) { //JARED
+      digitalWrite(ledPin2, HIGH);
+      delay(500);
+      digitalWrite(ledPin2, LOW);
+    }
+    break;
+  }
+  cmd = 0;
+  activateDoor(&leftState, 0, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
+  activateDoor(&rightState, 0, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+  //Serial.print(leftState); //debugging
+  //Serial.print("  <--Left  Right-->  ");
+  //Serial.println(rightState);
+  if (leftState == 100) { //JARED whole funtion
+    activateDoor(&leftState, 3, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //clear error
+  }
+  if (rightState == 100) { //JARED whole function
+    activateDoor(&rightState, 3, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //clear error
+  }
+}
 #endif
--- GarageDoor/GarageDoor.ino
+++ GarageDoor/GarageDoor.ino
@@ -3,14 +3,10 @@
 //                          DIRECTIVES
 //
 //***********************************************************************
-#define SHIELD_TYPE 0
+#define SHIELD_TYPE 1
 // ETHERNET = 0
 // WIFLY = 1
 // WIFI = 2
-
-#define DOOR_SENSOR_TYPE 0 //Jared, Peter: Change this to a 0 on yours.
-// 0:digital
-// 1:analog
 
 #define CRLF "\r\n"
 #define WIFLY_RECONFIGURATE 0 //Set to 1 to re-confiure the WiFly Shield with different credentials. 
@@ -23,7 +19,7 @@
 //***********************************************************************
 //Left door = the only door in a 1 door config
 //SET THIS TO HOW MANY DOORS YOU HAVE.  CURRENTLY SUPPORTS 1 OR 2
-static int doors = 2;
+static int doors = 1;
 
 boolean DoorOpen = false;
 boolean DoorClosed = false;
@@ -39,17 +35,10 @@ char pwd[16];
 unsigned long leftStateTimer;
 unsigned long rightStateTimer;
 
-#if DOOR_SENSOR_TYPE == 0
 int closeSensor1 = 3; // close sensor door 1
 int openSensor1 = 2; // open sensor door 1
 int closeSensor2 = 5; // close sensor door 2
 int openSensor2 = 4; // open sensor door 2
-#elif DOOR_SENSOR_TYPE == 1
-int analogOpenSensor1 = 0; // open analog sensor for Limit switches
-int analogOpenSensor2 = 1; // open analog sensor for Limit switches
-int analogCloseSensor1 = 2; // Close analog sensor for Limit switches
-int analogCloseSensor2 = 3; // Close analog sensor for Limit switches
-#endif
 
 int leftState = 0;
 int rightState = 0;
@@ -81,115 +70,6 @@ int loginattempt = 0; //login attempt counter
 
 #endif
 
-
-//*****************************PROCESS COMMANDS***************************************
-void processCommands() {
-  switch (cmd) {
-  case 1:
-    //Open command
-    if (leftState == 0) activateDoor(&leftState, 1, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
-    break;
-  case 2:
-    //Close command
-    if (leftState == 0) activateDoor(&leftState, 2, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
-    break;
-  case 3:
-    //toggle command
-    if (leftState == 0) { //JARED
-      digitalWrite(ledPin, HIGH);
-      delay(500);
-      digitalWrite(ledPin, LOW);
-    }
-    break;
-  case 4:
-    //Open door 2 command
-    if (rightState == 0) activateDoor(&rightState, 1, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
-    break;
-  case 5:
-    //close door 2 command
-    if (rightState == 0) activateDoor(&rightState, 2, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
-    break;
-  case 6:
-    //toggle door 2 command
-    if (rightState == 0) { //JARED
-      digitalWrite(ledPin2, HIGH);
-      delay(500);
-      digitalWrite(ledPin2, LOW);
-    }
-    break;
-  }
-  cmd = 0;
-  activateDoor(&leftState, 0, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
-  activateDoor(&rightState, 0, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
-  //Serial.print(leftState); //debugging
-  //Serial.print("  <--Left  Right-->  ");
-  //Serial.println(rightState);
-  if (leftState == 100) { //JARED whole funtion
-    activateDoor(&leftState, 3, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //clear error
-  }
-  if (rightState == 100) { //JARED whole function
-    activateDoor(&rightState, 3, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //clear error
-  }
-}
-
-
-
-//*****************************ACTIVATE COMMAND***************************************
-int activateDoor(int *doorState, int action,  unsigned long *stateTimer, boolean closed, boolean open, int buttonOutput)  { //JARED
-  switch (*doorState) {
-  case 0: //Default state.  If action is 1, begin opening door.  If action is 2, begin closing door.
-    if (action == 1) *doorState = 10; //Opening Door
-    if (action == 2) *doorState = 50; //Closing Door
-    *stateTimer = millis(); //entire process can take 20 seconds total
-    break;
-
-  case 10: //Door is idle.  If door not already open, trigger button, record timestamp, and move to next case.
-    if (open != true) { 
-      digitalWrite(buttonOutput, HIGH);
-      delay(500);
-      digitalWrite(buttonOutput, LOW);
-      delay(500);
-      *doorState = 20;
-    }
-    else if (open == true && closed == false) {
-      *doorState = 0;
-    }
-    break;
-
-  case 20: //Door is moving.  Wait for open or closed.  If open, set case to 0.  If closed, set case to 10.   If timeout expired, return 1
-    if (open == true && closed == false) *doorState = 0;
-    else if (millis() - *stateTimer > 20000) *doorState = 100;
-    else if (closed == true) *doorState = 10;
-    break;
-
-  case 50: //Door is idle.  If door not already closed, trigger button, record timestamp, and move to next case.
-    if (closed != true) { 
-      digitalWrite(buttonOutput, HIGH);
-      delay(500);
-      digitalWrite(buttonOutput, LOW);
-      delay(500);
-      *doorState =60;
-    }
-    else if (open == false && closed == true) {
-      *doorState = 0;
-    }
-    break;
-
-  case 60: //Door is moving.  Wait for open or closed.  If closed, set case to 0.  If oo, set case to 10.   If timeout expired, return 1
-    if (closed == true && open == false) *doorState = 0;
-    else if (millis() - *stateTimer > 20000) *doorState = 100;
-    else if (open == true) *doorState = 50;
-    break;
-
-  case 100: //Shit's wrong.  If action == 3, the error is ack'd
-    digitalWrite(errorLED, HIGH);
-    if (action == 3) *doorState = 0; //Ack'd
-    break;
-  }
-
-  return *doorState;
-}
-
 void setup() {
 
   pinMode(errorLED, OUTPUT);
@@ -200,7 +80,6 @@ void setup() {
 
 
   //Set internal pull-up resistors
-#if DOOR_SENSOR_TYPE == 0
   pinMode(openSensor1, INPUT);
   pinMode(closeSensor1, INPUT);
   pinMode(openSensor2, INPUT);
@@ -217,27 +96,6 @@ void setup() {
   if (digitalRead(closeSensor1) == LOW) DoorClosed = true;
   if (digitalRead(openSensor2) == LOW) Door2Open = true;
   if (digitalRead(closeSensor2) == LOW) Door2Closed = true; 
-#elif DOOR_SENSOR_TYPE == 0
-  //Set internal pull-up resistors
-  pinMode(analogOpenSensor1, INPUT);
-  pinMode(analogCloseSensor1, INPUT);
-  pinMode(analogOpenSensor2, INPUT);
-  pinMode(analogOpenSensor2, INPUT);
-
-  //By writing HIGH to input pins, we sets the pull-up resistors
-  digitalWrite(analogOpenSensor1, HIGH);
-  digitalWrite(analogCloseSensor1, HIGH);
-  digitalWrite(analogOpenSensor2, HIGH);
-  digitalWrite(analogOpenSensor2, HIGH);
-
-  //Prevent arduino from tweeting on reset.
-  if (isHigh(analogOpenSensor1) == false) DoorOpen = true;
-  if (isHigh(analogCloseSensor1) == false) DoorOpen = true;
-  if (isHigh(analogOpenSensor2) == false) Door2Open = true;
-  if (isHigh(analogOpenSensor2) == false) Door2Open = true;
-#endif
-
-
 
 #if SHIELD_TYPE == 0
   EthernetSetup();

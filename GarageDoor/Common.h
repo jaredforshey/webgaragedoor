bd0bf8085922e5f7afcc72e1e5d16d5cd6a0e247
Jared Forshey <forshey@gmail.com>
2013-03-14 19:29:28 -0400

    Fixed debounce issue with both garage doors
    Issue I was seeing with my right garage door was probably due to
    mounting of the mag sensor and wind vibrations of the door. I fixed the
    debounce delays on all state transitions. This really only affected the
    twitter writes, so it doesn't matter anymore anyway

--- GarageDoor/Common.h
+++ GarageDoor/Common.h
@@ -13,31 +13,38 @@
 //***************************** LEFT DOOR *************************************************************************
 void leftStatus(){
   //get garage door status
-  if ((digitalRead(openSensor1) == LOW) && (DoorOpen != true)) { //open
-    DoorClosed = false;
-    DoorOpen = true;
-    if (doors == 2 && (leftState == 0 || leftState == 20)) { //JARED
-      Serial.println("Left door opened");
-    } 
-    else if (leftState == 0 || leftState == 20) { //JARED
-      Serial.println("opened");
+  if ((digitalRead(openSensor1) == LOW) && (DoorOpen != true)) { //delay and check again for debounce
+    delay(500);
+    if ((digitalRead(openSensor1) == LOW) && (DoorOpen != true)){ //open
+      DoorClosed = false;
+      DoorOpen = true;
+      if (doors == 2 && (leftState == 0 || leftState == 20)) {
+        Serial.println("Left door opened");
+      } 
+      else if (leftState == 0 || leftState == 20) {
+        Serial.println("opened");
+      }
     }
   }
-  else if ((digitalRead(closeSensor1) == LOW) && (DoorClosed != true)) { //closed
-    DoorClosed = true;
-    DoorOpen = false;
-    if (doors == 2 && (leftState == 0 || leftState == 60)) { //JARED
-      Serial.println("Left door closed");
-    } 
-    else if (leftState == 0 || leftState == 60){ //JARED
-      Serial.println("closed");
+  else if ((digitalRead(closeSensor1) == LOW) && (DoorClosed != true)) { //delay and check again for debounce
+    delay(500);
+    if ((digitalRead(closeSensor1) == LOW) && (DoorClosed != true)){
+      DoorClosed = true;
+      DoorOpen = false;
+      if (doors == 2 && (leftState == 0 || leftState == 60)) {
+        Serial.println("Left door closed");
+      } 
+      else if (leftState == 0 || leftState == 60){
+        Serial.println("closed");
+      }
     }
   } 
   else if (digitalRead(closeSensor1) == HIGH && digitalRead(openSensor1) == HIGH  && (DoorClosed == true || DoorOpen == true)) {
-    delay(250);
+    delay(500);
     if (digitalRead(closeSensor1) == HIGH && digitalRead(openSensor1) == HIGH) {
       DoorClosed = false;
       DoorOpen = false;
+      Serial.println("Left Door Semi-open");
     }
   }
 }
@@ -48,23 +55,30 @@ void leftStatus(){
 void rightStatus() {
   //get door 2 status
   if (doors == 2) {
-    if ((digitalRead(openSensor2) == LOW) && (Door2Open != true)) {//open
-      Door2Closed = false;
-      Door2Open = true;
-      if (rightState == 0 || rightState == 20)
-      Serial.println("Right door opened");
+    if ((digitalRead(openSensor2) == LOW) && (Door2Open != true)) {//delay and check again for debounce
+      delay(500);
+      if ((digitalRead(openSensor2) == LOW) && (Door2Open != true)){ //open
+        Door2Closed = false;
+        Door2Open = true;
+        if (rightState == 0 || rightState == 20)
+        Serial.println("Right door opened");
+      }
     }
-    else if ((digitalRead(closeSensor2) == LOW) && (Door2Closed != true)) {//closed
-      Door2Closed = true;
-      Door2Open = false;
-      if (rightState == 0 || rightState == 60)
-      Serial.println("Right door closed");
+    else if ((digitalRead(closeSensor2) == LOW) && (Door2Closed != true)) { //delay and check again for debounce
+      delay(500);
+      if ((digitalRead(closeSensor2) == LOW) && (Door2Closed != true)){
+        Door2Closed = true;
+        Door2Open = false;
+        if (rightState == 0 || rightState == 60)
+        Serial.println("Right door closed");
+      }
     }
     else if (digitalRead(closeSensor2) == HIGH && digitalRead(openSensor2) == HIGH && (Door2Closed == true || Door2Open == true)) {
-      delay(250);
+      delay(500);
       if (digitalRead(closeSensor2) == HIGH && digitalRead(openSensor2) == HIGH) {
         Door2Closed = false;
         Door2Open = false;
+        Serial.println("Right Door Semi-open");
       }
     }
   }
@@ -132,15 +146,15 @@ void processCommands() {
   switch (cmd) {
   case 1:
     //Open command
-    if (leftState == 0) activateDoor(&leftState, 1, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
+    if (leftState == 0) activateDoor(&leftState, 1, &leftStateTimer, DoorClosed, DoorOpen, ledPin);
     break;
   case 2:
     //Close command
-    if (leftState == 0) activateDoor(&leftState, 2, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
+    if (leftState == 0) activateDoor(&leftState, 2, &leftStateTimer, DoorClosed, DoorOpen, ledPin);
     break;
   case 3:
     //toggle command
-    if (leftState == 0) { //JARED
+    if (leftState == 0) {
       digitalWrite(ledPin, HIGH);
       delay(500);
       digitalWrite(ledPin, LOW);
@@ -148,15 +162,15 @@ void processCommands() {
     break;
   case 4:
     //Open door 2 command
-    if (rightState == 0) activateDoor(&rightState, 1, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+    if (rightState == 0) activateDoor(&rightState, 1, &rightStateTimer, Door2Closed, Door2Open, ledPin2);
     break;
   case 5:
     //close door 2 command
-    if (rightState == 0) activateDoor(&rightState, 2, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+    if (rightState == 0) activateDoor(&rightState, 2, &rightStateTimer, Door2Closed, Door2Open, ledPin2);
     break;
   case 6:
     //toggle door 2 command
-    if (rightState == 0) { //JARED
+    if (rightState == 0) {
       digitalWrite(ledPin2, HIGH);
       delay(500);
       digitalWrite(ledPin2, LOW);
@@ -164,15 +178,15 @@ void processCommands() {
     break;
   }
   cmd = 0;
-  activateDoor(&leftState, 0, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //JARED
-  activateDoor(&rightState, 0, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //JARED
+  activateDoor(&leftState, 0, &leftStateTimer, DoorClosed, DoorOpen, ledPin);
+  activateDoor(&rightState, 0, &rightStateTimer, Door2Closed, Door2Open, ledPin2);
   //Serial.print(leftState); //debugging
   //Serial.print("  <--Left  Right-->  ");
   //Serial.println(rightState);
-  if (leftState == 100) { //JARED whole funtion
+  if (leftState == 100) {
     activateDoor(&leftState, 3, &leftStateTimer, DoorClosed, DoorOpen, ledPin); //clear error
   }
-  if (rightState == 100) { //JARED whole function
+  if (rightState == 100) {
     activateDoor(&rightState, 3, &rightStateTimer, Door2Closed, Door2Open, ledPin2); //clear error
   }
 }
